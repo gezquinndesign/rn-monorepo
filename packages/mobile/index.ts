6e193d6171e23d4e7fc3ad151aa02fd079f186cc
Gez Quinn <holla@gezquinn.design>
2022-04-11 20:50:51 +0100

    format

--- packages/mobile/index.ts
+++ packages/mobile/index.ts
@@ -1,6 +1,6 @@
-import { AppRegistry } from 'react-native'
-import { App } from 'components/src/App'
+import {AppRegistry} from 'react-native';
+import {App} from 'components/src/App';
 
-import { name as appName } from './app.json'
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App)
+AppRegistry.registerComponent(appName, () => App);
