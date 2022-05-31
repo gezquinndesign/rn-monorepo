e5b31ff28ba50e7f2789542b2c9a4a1e75d4a2b4
Gez Quinn <holla@gezquinn.design>
2022-05-31 23:09:25 +0100

    fix: get rid of octicons2

--- packages/components/src/libs/vector-icons/index.ts
+++ packages/components/src/libs/vector-icons/index.ts
@@ -1,19 +1,19 @@
-import { ThemeColors } from '../../../../core/src'
+import { IColors } from 'native-base/lib/typescript/theme/base/colors'
 
 import { MaterialIconProps } from './fonts/materialicons/MaterialIcons'
-import { OcticonIconProps } from './fonts/octicons2/Octicons2'
+import { OcticonIconProps } from './fonts/octicons/Octicons'
 import { EntypoIconProps } from './fonts/entypo/Entypo'
 import { FontAwesomeIconProps } from './fonts/fontawesome/FontAwesome'
 import { IoniconsIconProps } from './fonts/ionicons/Ionicons'
 
 export * from './fonts/materialicons/MaterialIcons'
-export * from './fonts/octicons2/Octicons2'
+export * from './fonts/octicons/Octicons'
 export * from './fonts/entypo/Entypo'
 export * from './fonts/fontawesome/FontAwesome'
 export * from './fonts/ionicons/Ionicons'
 
 export type IconProp = {
-  color?: keyof ThemeColors
+  color?: keyof IColors
 } & (
   | {
       family: 'octicon'
